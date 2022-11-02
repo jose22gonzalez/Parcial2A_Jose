49618bbd21aba01c2ebf5914ebde52899797bac7
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-11-02 19:32:57 -0400

    Mostrando Pantalla

--- app/src/main/java/edu/ucne/parcial2a_jose/MainActivity.kt
+++ app/src/main/java/edu/ucne/parcial2a_jose/MainActivity.kt
@@ -10,20 +10,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.parcial2a_jose.ui.Verbo.VerboList.VerboList
 import edu.ucne.parcial2a_jose.ui.theme.Parcial2A_JoseTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Parcial2A_JoseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+              VerboList()
             }
         }
     }
