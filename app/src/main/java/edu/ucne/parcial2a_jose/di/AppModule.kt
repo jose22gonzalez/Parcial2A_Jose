5425ae0334a9a569be7f4b34b17d92585f6e9789
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-11-02 18:44:37 -0400

    Creando la App Module

---
+++ app/src/main/java/edu/ucne/parcial2a_jose/di/AppModule.kt
@@ -0,0 +1,36 @@
+package edu.ucne.parcial2a_jose.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.parcial2a_jose.data.remote.apiverbo
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi{
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesVerboApi(moshi: Moshi): apiverbo {
+        return Retrofit.Builder()
+            .baseUrl("https://private-a127e-verbos.apiary-mock.com/verbos")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(apiverbo::class.java)
+    }
+}
